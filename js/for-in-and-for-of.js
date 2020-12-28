ae5dc21f38964c041dbfbbd3ddcb09c20c1db9e8
lzqMac <guai568@qq.com>
2020-12-28 20:21:54 +0800

    different between for in and for of

---
+++ js/for-in-and-for-of.js
@@ -0,0 +1,29 @@
+const str = '[()]'
+
+for (const i of str) {
+	console.log(i)
+}
+
+for (const i in str) {
+	console.log(i)
+}
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const i of arr) {
+	console.log(i)
+}
+
+for (const i in arr) {
+	console.log(i)
+}
+
+const obj = {
+	a: 1,
+	b: 2,
+	c: 3,
+}
+
+for (const i in obj) {
+	console.log(i)
+}
